3788826bf1fac945d83cba0945a55541681d9faa
SmockIsNotDead <abluecarrotjt@gmail.com>
2017-10-10 15:41:04 +0200

    Texto comentado
    no he visto funciones necesarias a parte de la principal, podrian hacerse pero no cumplirian su objetivo de simplificar.

--- prueba final/funcionesTrivial.js
+++ prueba final/funcionesTrivial.js
@@ -1,27 +1,40 @@
+
+//function fallada(temas[], tema){ 
+// se podrian hacer funciones por ejemplo para evaluar las respuestas de cada pregunta, pero no veo que lo vaya a hacer mas eficiente.
+
+
+
+/* No entra ni devuelve nada, se encarga de realizar la eleccion de preguntas.*/
 function menu(){
 	alert("Este programa automatiza la fase final del Trivial.");
 	var opcion;
 	var aciertos = 0;
 	var fallos = 0;
 	var respuesta;
+	/* creacion de variables para aciertos/fallos y para guardar la respuesta y opcion deseada.*/
 	alert(" Puede escoger entre varias opciones, para las cuales debe introducir su número correspondiente.");
-	//alert("0.Geografía; 1.Arte; 2.espectáculos; 3.Historia; 4.Ciencias; 5.Deportes.")
 
 	
 	
 	var temas=["0.Geografia","1.Arte","2.Espectáculos","3.Historia","4.Ciencias","5.Deportes"];
 	
+	/* bucle para que siga haciendo preguntas siempre que el jugador no haya 
+	ganado/perdido por exceso de fallos o aciertos*/
 	while((fallos < 3) && (aciertos < 4)){
 		
 		alert(temas);
 		opcion = prompt("Introduzca la opcion deseada.");
 		opcion = parseInt(opcion);
 
-
+		/* dependiendo de la opcion seleccionada se realiza una pregunta*/
 		switch(opcion){
 
 			case 0:
 				respuesta= prompt("¿En qué comunidad autónoma comenzaron los Sandkills de DAW2?");
+				/* Si la respuesta es acertada, se suma un acierto, sino se suma un fallo.
+				En ambos casos se elimina la ultima opcion seleccionada del array de opciones.
+				Con el añadido de que se mostrará si esa pregunta se acertó o falló.
+				*/
 				if (respuesta != "Cantabria"){
 					fallos++;
 					temas[0]= "Geografía fallada.";
@@ -112,6 +125,7 @@ function menu(){
 
 		}
 	}
+	/* Se notifica al usuario el motivo por el que ha terminado la prueba.*/
 	if(fallos >= 3 ){
 		alert("Has fracasado en la prueba.");
 	}else{
