9e787d3762477eac188ad7c48ba538d609bceaee
SmockIsNotDead <abluecarrotjt@gmail.com>
2017-11-08 02:02:54 +0100

    fix

--- 2.Objetos/Niger/u2e1_string.js
+++ 2.Objetos/Niger/u2e1_string.js
@@ -1,29 +1,47 @@
-function pedirDatos(){
+	function pedirDatos(){
 	var datos;
 	datos =prompt("Introduzca su nombre, dos apellidos, y el año de nacimiento separados por un espacio.");
 	var datosArray = datos.split(" ");
 
+	//pasamos la cadena a mayusculas
+	datos = pasarMayusculas(datos);
+
 	var nombre;
 	var apellidos;
 	var fechaNaci;
+	var apellido1;
+	var apellido2;
 
 	nombre = datosArray[0];
-	apellidos = datosArray[1] + datosArray[2];
+	apellidos = datosArray[1] +" "+ datosArray[2];
 	fechaNaci = datosArray[3];
+	apellido1 = datosArray[1];
+	apellido2 = datosArray[2];
+
 	nombreApellidos = nombre + apellidos;
 
+	alert(nombre +"\n"+ apellido1 +"\n"+ apellido2 +"\n"+ fechaNaci);
 	//variablepara longitud de nombre y apellidos
 	var longitud;
 	longitud = (nombreApellidos).length;
+	alert(nombreApellidos);
+	alert("La longitud de la cadena de nombre y apellidos es "+longitud);
+
 	contarVocales(nombreApellidos);
+
+	var nombreUsuario;
+	nombreUsuario = (nombre.charAt(0) + apellido1 + apellido2.charAt(0) + fechaNaci.slice(-2)).toLowerCase();
 
 
 
 }
 
+
+
 function contarVocales(cadena){
 	var cont = 0;
 	var num = 0;
+	var numA, numE, numI, numO, numU;
 	while (cont < 5){
 
 		switch (cont){
@@ -32,32 +50,39 @@ function contarVocales(cadena){
 				/* Split convierte el string en un array separado cada ve que encuentra el valor introducido(vocal)
 				De este modo luego cuento el tamaño del array-1 y asi cuento el numero de veces que se ha encontrado una vocal para hacer split
 				Por ejemplo: vocas  si lo separo por la a daria voc,as . Al ahcer el length daria 2 pero se ha encontrado solo una asi que -1.*/
-				alert("Tiene " + cadena.split('a').length - 1 + " a's");
-				num += (str.split('a').length - 1);
+				numA=cadena.split('a');
+				alert( cadena);
+				alert("Tiene " + (numA.length - 1) + " a's");
+				num += (numA.length - 1);
+				cont++;
 				break;
 
 			case 1:
-
-				alert("Tiene " + cadena.split('e').length - 1 + " e's");
-				num += (str.split('e').length - 1);
+				numE=cadena.split('e');
+				alert("Tiene " + (numE.length - 1) + " e's");
+				num += (numE.length - 1);
+				cont++;
 				break;
 
 			case 2:
 
-				alert("Tiene " + cadena.split('i').length - 1 + " i's");
-				num += (str.split('a').length - 1);
+				alert("Tiene " + (cadena.split('i').length - 1) + " i's");
+				num += (cadena.split('a').length - 1);
+				cont++;
 				break;
 
 			case 3:
 
-				alert("Tiene " + cadena.split('o').length - 1 + " o's");
-				num += (str.split('o').length - 1);
+				alert("Tiene " + (cadena.split('o').length - 1) + " o's");
+				num += (cadena.split('o').length - 1);
+				cont++;
 				break;
 
 			case 4:
 
-				alert("Tiene " + cadena.split('u').length - 1 + " u's");
-				num += (str.split('u').length - 1);
+				alert("Tiene " + (cadena.split('u').length - 1) + " u's");
+				num += (cadena.split('u').length - 1);
+				cont++;
 				break;
 		}
 
@@ -66,4 +91,8 @@ function contarVocales(cadena){
 	alert("En total tiene "+ num + " vocales.");
 
 
+}
+function pasarMayusculas(cadena){
+
+	return cadena.toUpperCase();
 }
