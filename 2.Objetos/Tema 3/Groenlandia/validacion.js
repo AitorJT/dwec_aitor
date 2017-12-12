be8e26f284994249a48748c4252b6752ea2ae1c9
SmockIsNotDead <abluecarrotjt@gmail.com>
2017-12-12 14:24:18 +0100

    buen trabajo github...

---
+++ 2.Objetos/Tema 3/Groenlandia/validacion.js
@@ -0,0 +1,139 @@
+
+var formulario = document.getElementById("miFormulario");
+
+window.onload = iniciar;
+
+function iniciar() {
+    document.getElementById("enviar").addEventListener('click', validar, false);
+    rForfait();
+    rSocio();
+}
+
+function soporta(){
+    if(typeof(Storage)!== "undefined"){
+        if(!localStorage.getItem("contForfait")){
+            localStorage.setItem("contForfait","0");
+        }
+        if(!localStorage.getItem("contSocio")){
+            localStorage.setItem("contSocio","0");
+        }
+    }
+}
+function contForfait(){
+    localStorage.setItem("contForfait",Number(localStorage.getItem("contForfait"))+1);
+    document.getElementById("contForfait").innerHTML = "Contador forfaits: "+ localStorage.getItem("contForfait");
+}
+function contSocio(){
+    localStorage.setItem("contSocio",Number(localStorage.getItem("contSocio"))+1);
+    document.getElementById("contSocio").innerHTML = "Contador forfaits: "+ localStorage.getItem("contSocio");
+}
+//funcion para añadir los cuatro campos al marcar el checkbox y borrarlos si se desmarca.
+function dynInput(cbox) {
+      if (cbox.checked) {
+        var input = document.createElement("input");
+        var input2 = document.createElement("input");
+        var input3 = document.createElement("input");
+        var input4 = document.createElement("input");
+        input.type = "number";
+        input2.type="radio";
+        input3.type="radio";
+        input4.type="radio";
+        var div = document.createElement("div");
+        var div2=document.createElement("div");
+        var div3=document.createElement("div");
+        var div4=document.createElement("div");
+        div.id = cbox.name;
+        div2.id = cbox.name+1;
+        div3.id = cbox.name+2;
+        div4.id = cbox.name+3;
+        div.min = 10000;
+        div.max = 99999;
+        /*div.required;
+        div.required=true;*/
+        div.innerHTML = "Numero de socio" + cbox.name;
+        div2.innerHTML = "Infantil";
+        div3.innerHTML = "Juvenil";
+        div4.innerHTML = "Senior";
+        div.appendChild(input);
+        div2.appendChild(input2);
+        div3.appendChild(input3);
+        div4.appendChild(input4);
+        document.getElementById("insertinputs").appendChild(div);
+        document.getElementById("insertinputs").appendChild(div2);
+        document.getElementById("insertinputs").appendChild(div3);
+        document.getElementById("insertinputs").appendChild(div4);
+      } else {
+        document.getElementById(cbox.name).remove();
+        document.getElementById(cbox.name+1).remove();
+        document.getElementById(cbox.name+2).remove();
+        document.getElementById(cbox.name+3).remove();
+      }
+    }
+function error(e, error) {
+    document.getElementById("mensajeError").innerHTML = error;
+    elemento.className = "error";
+    elemento.focus();
+}
+
+
+function validaNombre() {
+    var elemento = document.getElementById("nombre");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Venga, fijo que tienes un nombre.")
+        }
+        if (elemento.validity.patternMismatch) {
+            error(elemento, "Estoy seguro de que tu nombre tiene mas de 1 y menos de 16 caracteres.");
+        }
+        
+        return false;
+    }
+    return true;
+}
+function cuentaSocios(){
+    if(document.getElementById("socio").checked){
+        contSocio();
+    }
+}
+function borrarError() {
+    var formulario = document.forms[0];
+    for (var i = 0; i < formulario.elements.length; i++) {
+        formulario.elements[i].className = "";
+    }
+}
+function rSocio() {
+    document.getElementById("resetSocio").addEventListener('click', resetSocio, false);
+}
+function rForfait() {
+    document.getElementById("resetSocio").addEventListener('click', resetForfait, false);
+}
+function resetSocio(){
+    document.getElementById("contSocio").innerHTML = "";
+}
+function resetForfait(){
+    document.getElementById("contForfait").innerHTML = "";
+}
+function validaTelefono() {
+    var elemento = document.getElementById("telefono");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Debe introducir un teléfono")
+        }
+        if (elemento.validity.patternMismatch) {
+            error(elemento, "El telefono debe tener 9 numeros");
+        } 
+        return false;
+    }
+    return true;
+}
+function validar(e) {
+    borrarError();
+    if (validaNombre() && validaTelefono() && confirm("Pulsa aceptar si deseas enviar el formulario")) {
+        contForfait();
+        cuentaSocios();
+        return true
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
---
+++ 2.Objetos/Tema 3/TwinPeaks/validacion.js
@@ -0,0 +1,97 @@
+//formulario con:
+//poner patterns para autentificaciones investigadores:
+//nombre-2-32
+//num-0-9(8)+letra
+//idvirus num de 1 a 10 digitos
+//firma? 8-50string empieza por s4nd1eg0, escribir en focus automaticamente s4nd1eg0
+//al de 15 segundos emitir mensaje jessie pinkman
+var formulario = document.getElementById("miFormulario");
+
+window.onload = iniciar;
+//cuenta d 15 segundos sin hacer nada
+document.onload = setTimeout(function(){alert("no seas como Jessie Pinkman");},15000);
+
+function iniciar() {
+    document.getElementById("enviar").addEventListener('click', validar, false);
+    
+}
+
+function soporta(){
+    if(typeof(Storage)!== "undefined"){
+        if (localStorage.usuario!=null){
+            document.getElementById("saludo").innerHTML="Hola otra vez "+localStorage.usuario+;
+        }else{
+            localStorage.usuario=prompt("Introduce tu nuevo usuario");
+            document.getElementById("saludo").innerHTML="Bienvenido "+  localStorage.usuario+"!";
+        }
+        
+    }
+}
+
+
+function error(e, error) {
+    document.getElementById("mensajeError").innerHTML = error;
+    elemento.className = "error";
+    elemento.focus();
+}
+
+
+function validaNombre() {
+    var elemento = document.getElementById("nombre");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Venga, fijo que tienes un nombre.");
+        
+        
+        return false;
+    }
+    return true;
+}
+
+function validaNumero() {
+    var elemento = document.getElementById("identificador");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Sin ID no accedes, la vida es dura.");
+        
+        
+        return false;
+    }
+    return true;
+}
+    function validaVirus() {
+    var elemento = document.getElementById("ivirus");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Ya es dificil escoger un numero que no este entre 1 y 9999999999 eh?");
+        
+        
+        return false;
+    }
+    return true;
+}
+        
+//ni idea de que escriba automaticamente sandiego
+function validaSandiego() {
+    var elemento = document.getElementById("sandiego");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            error(elemento, "Si no la recuerdas tienes un problema");
+        }
+        if (elemento.validity.patternMismatch) {
+            error(elemento, "Debe empezar por S4ND1EG0");
+        } 
+        return false;
+    }
+    return true;
+}
+function validar(e) {
+    borrarError();
+    if (validaNombre() && validaNumero() && validaVirus() && validaSandiego() && confirm("Pulsa aceptar si deseas enviar el formulario")) {
+        
+        return true
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
